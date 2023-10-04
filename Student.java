a423422b63cdd56b345c5b682e2c80fdca6ae4e7
mihiri-karunarathne <karunarathnamihiri429@gmail.com>
2023-10-04 16:47:51 +0530

    first commit

---
+++ Student.java
@@ -0,0 +1,19 @@
+/*class Student{ 
+    int id; 
+    String name; 
+    Student (){
+    id = 23405;
+    name= "kiki";
+    }
+    void display(){
+    System.out.println(id+" "+name);
+} 
+public static void main(String args[]){ 
+//creating objects 
+Student s1=new Student(); 
+Student s2=new Student(); 
+//displaying values of the object 
+s1.display(); 
+s2.display(); 
+} 
+} */   
